307b466e65fd494df20f2ccc93e1a0799b9f75a4
goody-1 <atundeg@gmail.com>
2024-06-28 20:44:10 +0100

    add delete method, modify all and reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,8 +9,29 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    from models.base_model import BaseModel
+    from models.user import User
+    from models.place import Place
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.review import Review
+
+    __classes = {
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+            }
+
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+
+        if cls and cls in FileStorage.__classes:
+            temp = {}
+            for key, val in FileStorage.__objects.items():
+                if key.split('.')[0] == cls:
+                    temp[key] = val
+            return temp
         return FileStorage.__objects
 
     def new(self, obj):
@@ -28,26 +49,29 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as f:
                 try:
                     temp = json.load(f)
                     for key, val in temp.items():
                         cls_name = val['__class__']
-                        if cls_name in classes:
-                            self.all()[key] = classes[cls_name](**val)
+                        if cls_name in FileStorage.__classes:
+                            self.all()[key] = \
+                                FileStorage.__classes[cls_name](**val)
                 except json.JSONDecodeError:
                     raise ValueError
+
+    def delete(self, obj=None):
+        """Deletes an object (obj) from FileStorage.__objects"""
+
+        if obj and obj in FileStorage.__objects.values():
+            key_to_delete = None
+
+            for key, value in FileStorage.__objects.items():
+                if value == obj:
+                    key_to_delete = key
+
+            if key_to_delete:
+                del FileStorage.__objects[key_to_delete]
+                self.save()
